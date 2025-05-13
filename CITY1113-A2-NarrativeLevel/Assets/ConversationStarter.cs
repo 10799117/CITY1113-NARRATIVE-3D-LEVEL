bcd49a971cf9b99052ed74fd6849aabb1bfe57ad
10799117 <10799117@cityplym.ac.uk>
2025-05-13 11:37:53 +0100

    not working player dialogue stuff

--- CITY1113-A2-NarrativeLevel/Assets/ConversationStarter.cs
+++ CITY1113-A2-NarrativeLevel/Assets/ConversationStarter.cs
@@ -7,12 +7,15 @@ public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
 
+    public GameObject FPSController;
+
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+                FPScontroller.GetComponent<FPSController>().enabled = false;
                 ConversationManager.Instance.StartConversation(myConversation);
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
